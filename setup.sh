3db5661d8d3f53ee4b4afa2f2e52f5157b4f67ab
Ryan Miller <rmill@blacklinegps.com>
2013-07-23 21:28:48 -0600

    Added the emulationstation configs to the repo

--- setup.sh
+++ setup.sh
@@ -15,8 +15,9 @@ sudo git clone --depth=0 git://github.com/petrockblog/RetroPie-Setup.git /var/Re
 sudo chmod +x /var/RetroPie/retropie_setup.sh
 sudo /var/RetroPie/retropie_setup.sh
 sudo mkdir /home/pi/.emulationstation
-#sudo cp es_system.cfg /home/pi/.emulationstation
 sudo ln -s /home/pi/.emulationstation /.emulationstation
+#sudo ln -s es_system.cfg /.emulationstation/es_system.cfg
+sudo ln -s es_input.cfg /.emulationstation/es_input.cfg
 
 # Hide the Kernel Logging
 #cat cmdline.txt > /boot/cmdline.txt
@@ -28,14 +29,17 @@ sudo ln -s /home/pi/.emulationstation /.emulationstation
 #cp splash /etc/init.d
 
 # Add Init Scripts
-sudo cp emulation-station /etc/init.d/
-sudo cp nes-keypress /etc/init.d/
+sudo ln -s emulation-station /etc/init.d/emulation-station
+sudo ln -s nes-keypress /etc/init.d/nes-keypress
 sudo update-rc.d /etc/init.d/emulation-station defaults
 sudo update-rc.d /etc/init.d/nes-keypress defaults
 
 # Install Python Libraries
 sudo apt-get install python-rpi.gpio python3-rpi.gpio
-sudo cp nes-keypress.py /usr/bin/
+sudo ln -s nes-keypress.py /usr/bin/nes-keypress.py
+sudo mkdir /usr/bin/nes-keypress
+sudo ln -s controller1.json /usr/bin/nes-keypress/controller1.json
+sudo ln -s controller1.json /usr/bin/nes-keypress/controller2.json
 
 # Install the uinput library
 cd python-uinput-0.9
